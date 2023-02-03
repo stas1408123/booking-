6ebb7c571e00cfdf25b262056fdae3c3f3cb3572
blackberts <blackberts13@gmail.com>
2023-02-03 17:53:26 +0300

    Add properties to ViewModels

--- BKG/Booking.API/ViewModels/BookingViewModel.cs
+++ BKG/Booking.API/ViewModels/BookingViewModel.cs
@@ -2,5 +2,9 @@
 {
     public class BookingViewModel
     {
+        public decimal Price { get; set; }
+        public DateTime BookingFrom { get; set; }
+        public DateTime BookingTo { get; set; }
+        public string? Description { get; set; }
     }
 }
--- BKG/Booking.API/ViewModels/HotelViewModel.cs
+++ BKG/Booking.API/ViewModels/HotelViewModel.cs
@@ -2,5 +2,12 @@
 {
     public class HotelViewModel
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string Owner { get; set; }
+        public int Stars { get; set; }
+        public int CountRooms { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
