7ab3d1097747d79262537dfa89603f63ba921fd3
blackberts <blackberts13@gmail.com>
2023-02-06 14:18:10 +0300

    Updated DependencyInjection method

--- BKG/Booking.API/Program.cs
+++ BKG/Booking.API/Program.cs
@@ -1,17 +1,12 @@
 using Booking.API;
-using Booking.DAL;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMyDependencyGroup();
+builder.Services.AddDataAccessDependencies(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<BookingDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
