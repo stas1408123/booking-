22817fca633233f1fa94fde0df62cf8b9858a927
blackberts13@gmail.com <blackberts13@gmail.com>
2023-02-17 16:33:18 +0300

    Started to add integration tests

---
+++ BKG/Booking.IntegrationTests/Controllers/BookingControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using Booking.API;
+
+namespace Booking.IntegrationTests.Controllers;
+
+public class BookingControllerTest : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private readonly CustomWebApplicationFactory<Program> _factory;
+
+    public BookingControllerTest(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetAll()
+    {
+        // Act
+        var client = _factory.CreateClient();
+
+        // Arrange
+        var response = await client.GetAsync("api/Booking");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal("application/json; charset=utf-8",
+            response.Content.Headers.ContentType?.ToString());
+    }
+}
---
+++ BKG/Booking.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,24 @@
+﻿using Booking.DAL;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Booking.IntegrationTests;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram>
+    where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor =
+                services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<BookingDbContext>));
+
+            if (dbContextDescriptor != null) services.Remove(dbContextDescriptor);
+
+            services.AddDbContext<BookingDbContext>(options => options.UseInMemoryDatabase("InMemoryDbForTesting"));
+        });
+    }
+}
