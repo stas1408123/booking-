8989f97041e0f9dad7b8c3d70cc2e79419a57de5
blackberts <blackberts13@gmail.com>
2023-02-21 11:55:10 +0300

    add integration tests

---
+++ BKG/Booking.IntegrationTests/AutoData/BookingTestData.cs
@@ -0,0 +1,172 @@
+﻿using System.Text;
+using Booking.API;
+using Booking.DAL;
+using Booking.DAL.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace Booking.IntegrationTests.AutoData;
+
+public static class BookingTestData
+{
+    internal static BookingEntity ExpectedBooking => new()
+    {
+        BookingFrom = DateTime.Parse("2023-05-01"),
+        BookingTo = DateTime.Parse("2025-01-01"),
+        Price = 1,
+        Description = "Nvm",
+        HotelId = Guid.Parse("d990989f-bd61-450d-a6e9-b8eed2fd5ba2")
+    };
+
+    internal static BookingEntity ValidBookingToAdd => new()
+    {
+        BookingFrom = DateTime.Parse("2023-12-12"),
+        BookingTo = DateTime.Parse("2024-03-01"),
+        Price = 2,
+        Description = "Valid booking",
+        HotelId = Guid.Parse("d990989f-bd61-450d-a6e9-b8eed2fd5ba2")
+    };
+
+    internal static BookingEntity ValidBookingToUpdate => new()
+    {
+        Id = Guid.Parse("0c3db3ee-6f77-4b64-a5ec-27298749f421"),
+        BookingFrom = DateTime.Parse("2023-12-12"),
+        BookingTo = DateTime.Parse("2024-03-01"),
+        Price = 2,
+        Description = "Valid booking",
+        HotelId = Guid.Parse("d990989f-bd61-450d-a6e9-b8eed2fd5ba2")
+    };
+
+    internal static BookingEntity ValidGetParticularBookings => new()
+    {
+        HotelId = Guid.Parse("d990989f-bd61-450d-a6e9-b8eed2fd5ba2"),
+        BookingFrom = DateTime.Parse("2023-04-01"),
+        BookingTo = DateTime.Parse("2026-12-12")
+    };
+
+    internal static BookingEntity InvalidGetParticularBookings(string hotelId, string bookingTo, string bookingFrom)
+    {
+        return new BookingEntity
+        {
+            HotelId = Guid.Parse(hotelId),
+            BookingFrom = DateTime.Parse(bookingFrom),
+            BookingTo = DateTime.Parse(bookingTo)
+        };
+    }
+
+    internal static List<BookingEntity> ExpectedGetParticularBookings()
+    {
+        var bookingList = new List<BookingEntity>
+        {
+            new()
+            {
+                Id = Guid.Parse("00000000-0000-0000-0000-000000000000"),
+                BookingFrom = DateTime.Parse("2023-05-01"),
+                BookingTo = DateTime.Parse("2025-01-01"),
+                Price = 1,
+                Description = "Nvm",
+                HotelId = Guid.Parse("d990989f-bd61-450d-a6e9-b8eed2fd5ba2")
+            },
+            new()
+            {
+                Id = Guid.Parse("00000000-0000-0000-0000-000000000000"),
+                BookingFrom = DateTime.Parse("2023-06-01"),
+                BookingTo = DateTime.Parse("2024-01-01"),
+                Price = 2,
+                Description = "Nvm2",
+                HotelId = Guid.Parse("d990989f-bd61-450d-a6e9-b8eed2fd5ba2")
+            }
+        };
+
+        return bookingList;
+    }
+
+    internal static List<BookingEntity> ExpectedBookingList()
+    {
+        var bookingList = new List<BookingEntity>
+        {
+            new()
+            {
+                Id = Guid.Parse("0c3db3ee-6f77-4b64-a5ec-27298749f421"),
+                BookingFrom = DateTime.Parse("2023-05-01"),
+                BookingTo = DateTime.Parse("2025-01-01"),
+                Price = 1,
+                Description = "Nvm",
+                HotelId = Guid.Parse("d990989f-bd61-450d-a6e9-b8eed2fd5ba2")
+            },
+            new()
+            {
+                Id = Guid.Parse("819f9de9-10d3-4459-a950-1561a34f0b9d"),
+                BookingFrom = DateTime.Parse("2023-06-01"),
+                BookingTo = DateTime.Parse("2024-01-01"),
+                Price = 2,
+                Description = "Nvm2",
+                HotelId = Guid.Parse("d990989f-bd61-450d-a6e9-b8eed2fd5ba2")
+            },
+            new()
+            {
+                Id = Guid.Parse("aae87a10-736e-47c0-9dba-b8550f902d0c"),
+                BookingFrom = DateTime.Parse("2023-08-01"),
+                BookingTo = DateTime.Parse("2023-12-01"),
+                Price = 3,
+                Description = "Idk",
+                HotelId = Guid.Parse("60e6d76a-9c13-488b-afce-a3b21dbc3177")
+            }
+        };
+
+        return bookingList;
+    }
+
+    internal static BookingEntity InvalidBookingToUpdate(string id, string bookingFrom, string bookingTo,
+        string description, decimal price, string hotelId)
+    {
+        return new BookingEntity
+        {
+            Id = Guid.Parse(id),
+            BookingFrom = DateTime.Parse(bookingFrom),
+            BookingTo = DateTime.Parse(bookingTo),
+            Description = description,
+            Price = price,
+            HotelId = Guid.Parse(hotelId)
+        };
+    }
+
+    internal static BookingEntity InvalidBookingToAdd(string bookingFrom, string bookingTo, string description,
+        decimal price, string hotelId)
+    {
+        return new BookingEntity
+        {
+            BookingFrom = DateTime.Parse(bookingFrom),
+            BookingTo = DateTime.Parse(bookingTo),
+            Description = description,
+            Price = price,
+            HotelId = Guid.Parse(hotelId)
+        };
+    }
+
+    internal static async Task<BookingEntity> GetByIdBooking(HttpResponseMessage response)
+    {
+        return JsonConvert.DeserializeObject<BookingEntity>(await response.Content.ReadAsStringAsync());
+    }
+
+    internal static async Task<List<BookingEntity>> GetBookingList(HttpResponseMessage response)
+    {
+        return JsonConvert.DeserializeObject<List<BookingEntity>>(await response.Content.ReadAsStringAsync());
+    }
+
+    internal static StringContent NewStringContent(BookingEntity bookingEntity)
+    {
+        return new StringContent(JsonConvert.SerializeObject(bookingEntity), Encoding.UTF8,
+            "application/json");
+    }
+
+    internal static void ReinitializeDbForTests(CustomWebApplicationFactory<Program> factory)
+    {
+        using var scope = factory.Services.CreateScope();
+        var scopedServices = scope.ServiceProvider;
+        var db = scopedServices.GetRequiredService<BookingDbContext>();
+
+        db.Database.EnsureDeleted();
+        db.Database.EnsureCreated();
+    }
+}
---
+++ BKG/Booking.IntegrationTests/Controllers/HotelControllerTest.cs
@@ -0,0 +1,186 @@
+﻿using System.Net;
+using Booking.API;
+using Booking.IntegrationTests.AutoData;
+using FluentAssertions;
+
+namespace Booking.IntegrationTests.Controllers;
+
+public class HotelControllerTest : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private readonly CustomWebApplicationFactory<Program> _factory;
+
+    public HotelControllerTest(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetAll_HotelListIsExist_ReturnsValidList()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var expectedList = HotelTestData.ExpectedHotelList();
+        HotelTestData.ReinitializeDbForTests(_factory);
+
+        // Act
+        var response = await client.GetAsync("api/Hotel");
+        var returnedList = HotelTestData.GetHotelList(response).Result;
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        returnedList.Should().BeEquivalentTo(expectedList);
+    }
+
+    [Fact]
+    public async Task GetById_HotelIsExist_ReturnsValidHotel()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var expectedHotel = HotelTestData.ExpectedHotel;
+
+        // Act
+        var response = await client.GetAsync("api/Hotel/d990989f-bd61-450d-a6e9-b8eed2fd5ba2");
+        var returnedHotel = HotelTestData.GetByIdHotel(response).Result;
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        returnedHotel.Should().BeEquivalentTo(expectedHotel);
+    }
+
+    [Fact]
+    public async Task GetById_HotelIsNotExist_ReturnsInternalServerError()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var expectedHotel = HotelTestData.ExpectedHotel;
+
+        // Act
+        var response = await client.GetAsync("api/Hotel/7a38b367-68d0-4506-a6ad-6d717bb88913");
+        var returnedHotel = HotelTestData.GetByIdHotel(response).Result;
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        returnedHotel.Should().NotBeEquivalentTo(expectedHotel);
+    }
+
+    [Theory]
+    [InlineData("", "Valid", "Valid", 15, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "", "Valid", 15, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "", 15, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", -1, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 1111, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", -1, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", 6, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", 5, "375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", 5, "+37533686922521312312323232")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", 5, "+375")]
+    public async Task Add_HotelPropertiesIsNotValid_ReturnsInternalServerError(string title, string description,
+        string address, int countRooms, string owner, int stars, string phoneNumber)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var hotelAddRequest =
+            HotelTestData.InvalidHotelToAdd(title, description, address, countRooms, owner, stars, phoneNumber);
+        var stringContent = HotelTestData.NewStringContent(hotelAddRequest);
+
+        // Act
+        var response = await client.PostAsync("api/Hotel/add", stringContent);
+        var returnedHotel = HotelTestData.GetByIdHotel(response).Result;
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        returnedHotel.Should().NotBeEquivalentTo(hotelAddRequest);
+    }
+
+    [Fact]
+    public async Task Add_HotelPropertiesIsValid_ReturnsValidHotel()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var hotelAddRequest = HotelTestData.ValidHotelToAdd;
+        var stringContent = HotelTestData.NewStringContent(hotelAddRequest);
+
+        // Act
+        var response = await client.PostAsync("api/Hotel/add", stringContent);
+        var returnedHotel = HotelTestData.GetByIdHotel(response).Result;
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        returnedHotel.Should().BeEquivalentTo(hotelAddRequest);
+    }
+
+    [Theory]
+    [InlineData("", "Valid", "Valid", 15, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "", "Valid", 15, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "", 15, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", -1, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 1111, "Valid", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "", 5, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", -1, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", 6, "+375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", 5, "375336869225")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", 5, "+37533686922521312312323232")]
+    [InlineData("Valid", "Valid", "Valid", 15, "Valid", 5, "+375")]
+    public async Task Update_HotelPropertiesIsNotValid_ReturnsInternalServerError(string title, string description,
+        string address, int countRooms, string owner, int stars, string phoneNumber)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var hotelUpdateRequest =
+            HotelTestData.InvalidHotelToUpdate(title, description, address, countRooms, owner, stars, phoneNumber);
+        var stringContent = HotelTestData.NewStringContent(hotelUpdateRequest);
+
+        // Act
+        var response = await client.PostAsync("api/Hotel/add", stringContent);
+        var returnedHotel = HotelTestData.GetByIdHotel(response).Result;
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        returnedHotel.Should().NotBeEquivalentTo(hotelUpdateRequest);
+    }
+
+    [Fact]
+    public async Task Update_HotelPropertiesIsValid_ReturnsValidHotel()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var hotelUpdateRequest = HotelTestData.ValidHotelToUpdate;
+        var stringContent = HotelTestData.NewStringContent(hotelUpdateRequest);
+
+        // Act
+        var response = await client.PostAsync("api/Hotel/add", stringContent);
+        var returnedHotel = HotelTestData.GetByIdHotel(response).Result;
+        returnedHotel.Id = hotelUpdateRequest.Id;
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        returnedHotel.Should().BeEquivalentTo(hotelUpdateRequest);
+    }
+
+    [Fact]
+    public async Task Delete_HotelIsExist_ReturnsSuccess()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.DeleteAsync("api/Hotel/delete/d990989f-bd61-450d-a6e9-b8eed2fd5ba2");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task Delete_HotelIsNotExist_ReturnsInternalServerError()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.DeleteAsync("api/Hotel/delete/0c3db3ee-3277-4b64-a5ec-27298749f421");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+    }
+}
