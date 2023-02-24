befbde84269afc44cda4aa0a92136eb5bd617088
blackberts <103589743+blackberts@users.noreply.github.com>
2023-02-24 17:09:34 +0300

    Set up solution for Identity Server (#27)
    * Set up solution for Identity Server

---
+++ IdentityServer/IdentityServer/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddIdentityServer();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseIdentityServer();
+
+app.MapControllers();
+
+app.Run();
