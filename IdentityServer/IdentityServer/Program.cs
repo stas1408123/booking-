3788629113de91cb2a657a2aedf3f7fd46418f34
blackberts13@gmail.com <blackberts13@gmail.com>
2023-02-26 12:41:51 +0300

    Created and configured auth db context

--- IdentityServer/IdentityServer/Program.cs
+++ IdentityServer/IdentityServer/Program.cs
@@ -1,6 +1,25 @@
+using IdentityServer.Data;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(opt =>
+    {
+        opt.Password.RequireDigit = false;
+        opt.Password.RequireLowercase = false;
+        opt.Password.RequireUppercase = false;
+        opt.Password.RequireNonAlphanumeric = false;
+        opt.Password.RequiredLength = 4;
+    })
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AuthDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
