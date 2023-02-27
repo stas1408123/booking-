ec1bcc2d5e9dce156e73f727e6bb4c314a6b0930
blackberts <103589743+blackberts@users.noreply.github.com>
2023-02-27 15:35:56 +0300

    Set up configuration for is (#30)
    * Set up configuration for IdentityServer

---
+++ IdentityServer/IdentityServer/Configuration.cs
@@ -0,0 +1,47 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+
+namespace IdentityServer;
+
+public static class Configuration
+{
+    public static IEnumerable<IdentityResource> GetIdentityResources()
+    {
+        return new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+    }
+
+    public static IEnumerable<ApiResource> GetApiResources()
+    {
+        return new List<ApiResource>
+        {
+            new("bookingAPI")
+        };
+    }
+
+    public static IEnumerable<Client> GetClients()
+    {
+        return new List<Client>
+        {
+            new()
+            {
+                ClientId = "client_id",
+                ClientSecrets = { new Secret("client_secret".ToSha256()) },
+
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = { "bookingAPI" }
+            }
+        };
+    }
+
+    public static IEnumerable<ApiScope> GetApiScopes()
+    {
+        return new ApiScope[]
+        {
+            new("bookingAPI")
+        };
+    }
+}
