e57a8b9e35af4900f5190e6b62b5f6ea25cd2671
blackberts <103589743+blackberts@users.noreply.github.com>
2023-03-03 20:44:32 +0300

    Add auth controller with login/register and logout logic (#34)

---
+++ IdentityServer/IdentityServer/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authentication;
+
+namespace IdentityServer.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        public string? Password { get; set; }
+    }
+}
