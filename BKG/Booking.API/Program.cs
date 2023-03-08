d77783db49756c0f052abf538cc95167d1b44c46
blackberts <blackberts13@gmail.com>
2023-03-08 14:16:13 +0300

    updated swagger to make authorized requests and etc

--- BKG/Booking.API/Program.cs
+++ BKG/Booking.API/Program.cs
@@ -1,5 +1,9 @@
 using Booking.API.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Booking.API;
 
@@ -9,25 +13,16 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        IdentityModelEventSource.ShowPII = true;
+
         builder.Services.AddApiAccessDependencies(builder.Configuration);
         builder.Services.AddCustomCors();
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddCustomSwaggerGen();
 
-        builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.Authority = "https://localhost:7291";
-                options.RequireHttpsMetadata = false;
-                options.Audience = "BookingAPI";
-            });
+        builder.Services.AddCustomAuthentication();
         builder.Services.AddAuthorization();
 
         var app = builder.Build();
@@ -36,7 +31,12 @@ public class Program
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(options =>
+            {
+                options.DocExpansion(DocExpansion.List);
+                options.OAuthClientId("client_id_booking");
+                options.OAuthClientSecret("client_secret_booking");
+            });
         }
 
         app.UseCors("MyPolicy");
@@ -61,10 +61,81 @@ internal static class CustomExtensionsMethods
         services.AddCors(options =>
         {
             options.AddPolicy("MyPolicy", builder => builder
-                .WithOrigins("https://localhost:7291")
+                .WithOrigins("https://localhost:10001")
                 .AllowAnyHeader()
                 .AllowAnyMethod());
         });
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomSwaggerGen(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Description = "Swagger API v1",
+                Title = "Swagger with Duende.IdentityServer",
+                Version = "1.0.0"
+            });
+
+            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows
+                {
+                    Password = new OpenApiOAuthFlow
+                    {
+                        TokenUrl = new Uri("https://localhost:5001/connect/token"),
+                        Scopes = new Dictionary<string, string>
+                        {
+                            { "BookingAPI", "Booking API" }
+                        }
+                    }
+                }
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "oauth2"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    },
+                    new List<string>()
+                }
+            });
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomAuthentication(this IServiceCollection services)
+    {
+        services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "https://localhost:5001";
+                options.RequireHttpsMetadata = false;
+                options.Audience = "BookingAPI";
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false
+                };
+            });
 
         return services;
     }
